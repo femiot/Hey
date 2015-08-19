31ff9911b1929dc43e145cc0b5694ece064a42c9
femiot <femiot>
2015-08-19 08:00:24 +0200

    db

---
+++ HudsonTest.Database/dbo/Tables/Maths.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Maths] (
+    [Id]   INT          IDENTITY (1, 1) NOT NULL,
+    [Time] VARCHAR (20) NOT NULL,
+    [Name] VARCHAR (20) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
